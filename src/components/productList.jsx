db0c919800f1483f687200320a55e6f590f32518
Bernardo B. Marques <bernardobarreto.dev@gmail.com>
2023-08-06 13:24:43 -0300

    get product list from api

--- src/components/productList.jsx
+++ src/components/productList.jsx
@@ -1,6 +1,28 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
 const ProductList = () => {
+  const [list, setList] = useState([]);
+
+  const getListFromApi = async () => {
+    const { data } = await axios.get(`https://fakestoreapi.com/products`)
+    setList(data)
+    console.log('Product list: ', data);
+  }
+
+  useEffect(() => {
+    getListFromApi()
+  }, [])
+
   return (
-    <h1>Product Listing</h1>
+    <>
+      <h1>Product Listing</h1>
+      <ul>
+        {list.map((product, index) => (
+          <li key={index}>{product.title}</li>
+        ))}
+      </ul>
+    </>
   )
 }
 
