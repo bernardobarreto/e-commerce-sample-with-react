0a8b3e329803e4fd89c5819df373083145f6924b
Bernardo B. Marques <bernardobarreto.dev@gmail.com>
2023-08-06 12:46:50 -0300

    generate with vite

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import './App.css'
+import ProductList from './components/productList'
+
+function App() {
+  return (
+    <ProductList/>
+  )
+}
+
+export default App
---
+++ src/components/productList.jsx
@@ -0,0 +1,7 @@
+const ProductList = () => {
+  return (
+    <h1>Product Listing</h1>
+  )
+}
+
+export default ProductList
