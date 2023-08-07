df034a10c7871700247a9dae118f7c720d35cb36
Bernardo B. Marques <bernardobarreto.dev@gmail.com>
2023-08-07 15:57:51 -0300

    add route and link for product details

--- src/appRoutes.jsx
+++ src/appRoutes.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Route, Routes, BrowserRouter } from 'react-router-dom'
 
 import ProductList from './components/productList'
+import ProductDetails from './components/productDetails'
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<ProductList/>} path="/" exact />
+        <Route element={<ProductDetails/>} path="/products/:id" />
       </Routes>
     </BrowserRouter>
   )
--- src/components/productCard.jsx
+++ src/components/productCard.jsx
@@ -1,13 +1,13 @@
+import { Link } from 'react-router-dom'
+
 const ProductCard = ({ attrs }) => {
   return (
     <div className='border-2'>
-      <a href='#'>
-        <p className='text-center m-2 truncate'>{attrs.title}</p>
-        <img src={attrs.image} className='h-40 mx-auto m-2'/>
-        <p className='text-right m-2'>${attrs.price}</p>
-      </a>
+      <p className='text-center m-2 truncate'>{attrs.title}</p>
+      <img src={attrs.image} className='h-40 mx-auto m-2'/>
+      <p className='text-right m-2'>${attrs.price}</p>
       <p className='m-2 p-1 text-xl bg-blue-600 hover:bg-blue-800 text-center rounded text-white hover:cursor-pointer'>
-        <a href="#">Add to cart</a>
+        <Link to={`/products/${attrs.id}`}>Product details</Link>
       </p>
     </div>
   )
