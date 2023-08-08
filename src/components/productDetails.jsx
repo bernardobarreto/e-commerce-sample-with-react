83c1178fcdc24f506cc82c75bd05bbe935f0542e
Bernardo B. Marques <bernardobarreto.dev@gmail.com>
2023-08-07 21:13:32 -0300

    change for addtoCart button after clicked the first time

--- src/components/productDetails.jsx
+++ src/components/productDetails.jsx
@@ -3,6 +3,7 @@ import { useQuery } from 'react-query'
 import { useDispatch, useSelector } from 'react-redux'
 import { addItem as cartAddItem } from '../features/cartSlice'
 import axios from 'axios'
+import { useState } from 'react'
 
 const ProductDetails = () => {
   const { id } = useParams()
@@ -14,10 +15,16 @@ const ProductDetails = () => {
     return data
   }
 
+  const cartItems = useSelector(state => state.cart.items)
+
   const { isLoading, error, data: attrs } = useQuery('productDetails', fetchProduct)
 
   if (isLoading) return 'Loading product details...'
   if (error) return 'An error ocurred: ' + error.message
+
+  const itemIsOnCart = attrs?.id && cartItems?.some((item) => item.id === attrs.id)
+  const addToCartText = itemIsOnCart ? 'Add one more to cart' : 'Add to cart'
+  const cartItemQuantity = itemIsOnCart ? cartItems.filter(item => item.id === attrs.id).length : 0
 
   return (
     <div className="max-w-md mx-auto">
@@ -29,11 +36,13 @@ const ProductDetails = () => {
         <div>
           <p>{attrs.description}</p>
           <p className="text-2xl underline my-4">${attrs.price}</p>
+          <p>Current product on cart: {cartItemQuantity}</p>
           <button
             className="my-4 py-1 px-4 text-xl bg-blue-600 hover:bg-blue-800 text-center rounded text-white hover:cursor-pointer"
-            onClick={() => dispatch(cartAddItem(attrs.id))}
+            onClick={() => dispatch(cartAddItem(attrs))}
             >
-              Add to cart</button>
+              {addToCartText}
+            </button>
           <p className='underline py-4'><Link to='/'>Go back to home page</Link></p>
         </div>
       </div>
