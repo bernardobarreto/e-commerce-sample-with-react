1817a361f54ba403e326e323be0bd9d4980d4994
Bernardo B. Marques <bernardobarreto.dev@gmail.com>
2023-08-11 13:19:08 -0300

    show list of items on cart view

--- src/components/cart.jsx
+++ src/components/cart.jsx
@@ -1,8 +1,26 @@
+import { useSelector } from "react-redux"
+
 const Cart = () => {
+  const selector = useSelector((state) => state.cart.items)
+  console.log(selector);
+
   return (
-    <>
-      <h1>Cart</h1>
-    </>
+    <div className='max-w-md mx-auto'>
+      <h1 className='text-center text-3xl'>Cart</h1>
+      {selector.map((product, index) => (
+        <div key={index}>
+          <div className='grid grid-cols-2 border p-4'>
+            <div>
+              <img src={product.image} alt="" className='h-20'/>
+            </div>
+            <div>
+              <p>{product.title}</p>
+              <p className='text-right'>${product.price}</p>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
   )
 }
 
